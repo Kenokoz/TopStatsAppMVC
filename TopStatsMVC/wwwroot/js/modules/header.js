35ccd8183f56d53016f9722d28663b70dd58af20
Kirill Andreev <kreizi2398@mail.ru>
2021-02-05 17:07:00 +0300

    Removing unused files

--- TopStatsMVC/wwwroot/js/modules/header.js
+++
@@ -1,7 +0,0 @@
-const links = document.querySelectorAll(".nav__link");
-
-links.forEach(link => {
-    link.addEventListener("click", () => {
-        link.classList.add("nav__active");
-    });
-});
